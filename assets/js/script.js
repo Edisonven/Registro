6e9c956321eb7e44f697722f3e5df58eabd8210f
Edison <aalevenegass@gmail.com>
2023-11-28 18:38:41 -0300

    creando un mensaje que aparece luego de que se registran

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,18 @@ const input_1 = document.getElementById("input-1");
 const select_1 = document.getElementById("select-1");
 const select_2 = document.getElementById("select-2");
 const boton = document.getElementById("submit__btn");
+const registrado = document.getElementById("registered");
 const invitados = [];
 
 boton.addEventListener("click", () => {
   let inputInvitados = input_1.value;
   let vegetariano = select_1.value;
   let asistencia = select_2.value;
-  if (inputInvitados === "" || vegetariano === "" || asistencia === "") {
+  if (
+    inputInvitados === "" ||
+    vegetariano.value == "" ||
+    asistencia.value == ""
+  ) {
     alert("Debes Rellenar todos los campos");
   } else {
     invitados.push({
@@ -17,6 +22,10 @@ boton.addEventListener("click", () => {
       asistencia: asistencia,
     });
     input_1.value = "";
+    registrado.innerHTML = `<b>¡TE HAS REGISTRADO!</b> Después de la fiesta, nos iremos de luna de miel a Europa. si quieres 
+    aportanos para poder realizar nuestro sueño, puedes hacerlo a las siguientes cuenta
+    cta. Corriente Banco ITAU: <b>210972591</b> RUT: <b>18.764.812-2</b>. También en la fiesta habrá un buzón. 
+    <b>¡Te esperamos!</b>`;
   }
 
   console.log(invitados);
